7a9309f444b8cf0523359048286285d66c33313a
Macflash <alex.brofo@outlook.com>
2015-03-23 14:56:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -12,11 +12,12 @@ function createroadwaydiv(rdiv,rway){
   for(row = 0; row < rway.length; row++){
       //add a row div
       var newdiv = document.createElement("div");
-      newdiv.style.class = "rowmarker";
+      newdiv.setAttribute('class', 'rowmarker');
       for(lane = 0; lane < rway.numLanes; lane++){
       //add some lane-marker divs
         var newtile = document.createElement("div");
-        newtile.style.class = "tilemarker";
+        newtile.setAttribute('class', 'tilemarker');
+        newtile.innerHTML = "-";
         newdiv.appendChild(newtile);
       }
       rdiv.appendChild(newdiv);
