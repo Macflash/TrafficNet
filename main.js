6d0696f35ef6a51aaae9ae64aebc5b8abc8b5149
Macflash <alex.brofo@outlook.com>
2015-03-23 14:31:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+var roadwaydiv;
+var rw = roadway(3,100);
+var dcar = dumbcar(1,50,10);
 
+function init(){
+  roadwaydiv = document.getElementById('roadway');
+  dcar.draw(rw);
+  
+} 
+
+function createroadwaydiv(rdiv,rway){
+  for(lane = 0; lane < rway.numLanes; lane++){
+    for(row = 0; row < rway.length; row++){
+      //add a row div
+      //add some lane-marker divs
+    }
+  }
+}
+
+function displayroadway(rdiv,rway){
+  
+}
