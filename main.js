adb84c5da087ad0d87251e98ac31d705ea258f52
Macflash <alex.brofo@outlook.com>
2015-03-23 15:55:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ function init(){
   roadwaydiv = document.getElementById('roadway');
   gamerw = new roadway(4,300);
   gamecars = new Array();
-  gamecars.push(new dumbcar(1,20,10));
+  gamecars.push(new dumbcar(1,20,15));
+  gamecars.push(new dumbcar(0,50,15));
+  gamecars.push(new dumbcar(3,100,15));
+  gamecars.push(new dumbcar(1,60,20));
+  gamecars.push(new dumbcar(4,200,40));
   
   createroadwaydiv(roadwaydiv,gamerw);
   
