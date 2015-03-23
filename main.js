80b8551738407589f401e08ea4733519dafd3bf3
Macflash <alex.brofo@outlook.com>
2015-03-23 16:06:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function init(){
   gamecars.push(new dumbcar(0,50,15));
   gamecars.push(new dumbcar(3,100,15));
   gamecars.push(new dumbcar(1,60,20));
-  gamecars.push(new dumbcar(4,200,40));
+  gamecars.push(new dumbcar(2,200,40));
   
   console.log(gamecars);
   
