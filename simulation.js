099c2f95b3b929c2370207bd1f904e2c86419500
Macflash <alex.brofo@outlook.com>
2015-03-23 15:19:32 -0500

    Update simulation.js

--- simulation.js
+++ simulation.js
@@ -1,1 +1,23 @@
+var gamecars = new Array();
+gamecars.push(new dumbcar(0,0,10));
 
+function update(){
+  //clear roadway
+  gamerw.reset();
+  
+  //draw cars
+  drawCars(gamecars, gamerw);
+  
+  //car decisions
+  
+  //car movement
+  
+  //check for collisions?
+  
+}
+
+function drawCars(cars,rw){
+  for(i=0;<cars.length;i++){
+    cars[i].draw(rw);
+  }
+}
