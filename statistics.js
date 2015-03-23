b6b82d0d3986230618f33c8af470b3899fe2de88
Macflash <alex.brofo@outlook.com>
2015-03-23 17:53:05 -0500

    Update statistics.js

--- statistics.js
+++ statistics.js
@@ -1,3 +1,27 @@
-function computeStats(cars){
+function typestat(cartype){
+  this.cartype = cartype;
+  this.spdsum = 0;
+  this.num = 0;
+  this.avg = function(){
+    return spdsum/num;
+  }
+  this.add = function(car){
+    this.spdsum = this.spdsum + car.speed;
+    this.num++;
+  }
+}
 
+function computeStats(cars){
+  var fast = new typestat("fast");
+  var dumb = new typestat("dumb");
+  for(i = 0; i < cars.length; i++){
+    if(cars[i].cartype == "fast"){
+      fast.add(cars[i]);
+    }
+    else{
+      dumb.add(cars[i]);
+    }
+  }
+  console.log("fast: " + fast.avg);
+  console.log("slow: " + slow.avg);
 }
