201a76c72ad289a0714b92c9e21bc51943800610
Macflash <alex.brofo@outlook.com>
2015-03-23 14:19:40 -0500

    Update roadway.js
    added basic roadway and dumbcar objects

--- roadway.js
+++ roadway.js
@@ -1,1 +1,40 @@
+//object to hold collision information for a roadway
+function roadway(lanes,length){
+  this.numLanes = lanes;
+  this.length = length;
+  this.lanes = new Array(lanes);
+  for(i = 0; i < this.numLanes; i++){
+    this.lanes[i] = new Array(this.length);
+  }
+  this.reset = function(){
+    this.lanes = new Array(lanes);
+    for(i = 0; i < this.numLanes; i++){
+      this.lanes[i] = new Array(this.length);
+    } 
+  }
+}
 
+function dumbcar(lane,location,length){
+  this.lane = lane;
+  this.location = location;
+  this.length = length;
+  this.speed = 1;
+  
+  this.draw = function(roadway){
+    for(i = 0; i < length; i++){
+      var s = location + i;
+      if(s > roadway.length){
+        s = s - roadway.length;
+      }
+      roadway.lanes[lane].[s] = speed;
+    }
+  }
+  
+  this.drive = function(roadway){
+    location = location + speed;
+    if(location >= roadway.length){
+      location = location - roadway.length;
+    }
+  }
+  
+}
