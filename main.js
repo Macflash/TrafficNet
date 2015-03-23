832e23f103ea5b1b5cc89a4d7ac8cfa483d7dd2a
Macflash <alex.brofo@outlook.com>
2015-03-23 17:34:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var gamecars;
 
 function init(){
   roadwaydiv = document.getElementById('roadway');
-  gamerw = new roadway(4,200);
+  gamerw = new roadway(4,300);
   gamecars = new Array();
   
   
