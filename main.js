1fa8e187c415252b111af210f2b67539b6d476f5
Macflash <alex.brofo@outlook.com>
2015-03-23 15:36:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ function init(){
   gamecars.push(new dumbcar(0,0,10));
   
   createroadwaydiv(roadwaydiv,gamerw);
-  console.log(gamerw);
-  console.log(gamerw.lanes[1]);
-  console.log(gamerw.lanes[1][1]);
+  
+  //console.log(gamerw);
+  //console.log(gamerw.lanes[1]);
+  //console.log(gamerw.lanes[1][1]);
+  
+  console.log(getTile(1,1));
+  
   update(gamerw,gamecars,roadwaydiv);
   setInterval("update(gamerw,gamecars,roadwaydiv)", 100);
 }
@@ -20,14 +24,21 @@ function createroadwaydiv(rdiv,rway){
       //add a row div
       var newdiv = document.createElement("div");
       newdiv.setAttribute('class', 'rowmarker');
+      newdiv.setAttribute('name', row);
       for(lane = 0; lane < rway.numLanes; lane++){
       //add some lane-marker divs
         var newtile = document.createElement("div");
         newtile.setAttribute('class', 'tilemarker');
+        newtile.setAttribute('name', lane);
         newdiv.appendChild(newtile);
       }
       rdiv.appendChild(newdiv);
   }
+}
+
+function getTile(row,lane){
+  var row = roadwaydiv.getElementByName(row);
+  var tile = row.getElementByName(lane);
 }
 
 function displayroadway(rdiv,rway){
